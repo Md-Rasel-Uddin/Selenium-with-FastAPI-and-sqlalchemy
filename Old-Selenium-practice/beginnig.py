5fd8ae1c9a2e8af3d4b4bff9c4f0cf8a8d1143d5
Md Rasel Uddin <raseluddin102@gmail.com>
2025-09-27 10:28:21 +0600

    Update beginnig.py

--- Old-Selenium-practice/beginnig.py
+++ Old-Selenium-practice/beginnig.py
@@ -1,1 +1,12 @@
 
+from selenium import webdriver
+
+# create a Chrome browser instance
+driver = webdriver.Chrome()
+
+# open a webpage
+driver.get("https://www.google.com")
+
+print(driver.title)
+
+driver.quit()
